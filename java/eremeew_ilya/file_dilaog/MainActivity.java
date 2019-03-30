734dc6af9d779bacee2877d0885ac0f5b8dbaf8f
eremeevilya <eremeew.ilya@yandex.ru>
2019-03-30 17:20:50 +0400

    test version 1

--- java/eremeew_ilya/file_dilaog/MainActivity.java
+++ java/eremeew_ilya/file_dilaog/MainActivity.java
@@ -2,12 +2,24 @@ package eremeew_ilya.file_dilaog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import eremeew_ilya.file_dilaog.file_dialog.FileDialogFragment;
+
+public class MainActivity extends AppCompatActivity
+{
+    private FileDialogFragment fileDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fileDialog = new FileDialogFragment();
+    }
+
+    public void openDialog(View view)
+    {
+        fileDialog.show(getSupportFragmentManager(), "fileDialog");
     }
 }
--- java/eremeew_ilya/file_dilaog/file_dialog/FileDialogFragment.java
+++ java/eremeew_ilya/file_dilaog/file_dialog/FileDialogFragment.java
@@ -3,19 +3,24 @@ package eremeew_ilya.file_dilaog.file_dialog;
 
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.File;
 
 import eremeew_ilya.file_dilaog.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class FileDialogFragment extends DialogFragment
 {
+    private TextView tv_path;
+    private ListView listFiles;
 
+    private FilesSD_Adapter adapter;
 
     public FileDialogFragment() {
         // Required empty public constructor
@@ -24,9 +29,37 @@ public class FileDialogFragment extends DialogFragment
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_file_dialog, container, false);
+                             Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_file_dialog, container, false);
+
+        if(!FilesSD_Adapter.isSD_Enable())
+        {
+            dismiss();
+            return view;
+        }
+
+        adapter = new FilesSD_Adapter(getContext());
+
+        tv_path = (TextView)view.findViewById(R.id.tv_path);
+        listFiles = (ListView)view.findViewById(R.id.listFiles);
+
+        listFiles.setOnItemClickListener(itemClickListener);
+
+        tv_path.setText(adapter.getRootPath().getAbsolutePath());
+        listFiles.setAdapter(adapter);
+
+        return view;
     }
 
+    private AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+        {
+            File file = adapter.getFile(i);
+
+            if(adapter.setRootPath(file))
+                tv_path.setText(file.getAbsolutePath());
+        }
+    };
 }
