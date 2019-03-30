9e94beb8393d236a3a4b6810a46da7b72b4d9af3
eremeevilya <eremeew.ilya@yandex.ru>
2019-03-30 16:03:31 +0400

    create project

---
+++ java/eremeew_ilya/file_dilaog/file_dialog/FileDialogFragment.java
@@ -0,0 +1,32 @@
+package eremeew_ilya.file_dilaog.file_dialog;
+
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import eremeew_ilya.file_dilaog.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FileDialogFragment extends DialogFragment
+{
+
+
+    public FileDialogFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_file_dialog, container, false);
+    }
+
+}
